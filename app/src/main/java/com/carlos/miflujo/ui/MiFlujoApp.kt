95d42694124f21e7587a9c4fdb59cfc055c770ce
carlosbenavides23 <carlosbenavidessandino@gmail.com>
2026-05-09 09:08:04 -0600

    style: fix dark mode snackbar colors

--- app/src/main/java/com/carlos/miflujo/ui/MiFlujoApp.kt
+++ app/src/main/java/com/carlos/miflujo/ui/MiFlujoApp.kt
@@ -13,9 +13,11 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -130,7 +132,13 @@ fun MiFlujoApp() {
             }
         },
         snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
+            SnackbarHost(hostState = snackbarHostState) { snackbarData ->
+                Snackbar(
+                    snackbarData = snackbarData,
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+            }
         },
     ) { innerPadding ->
         Box(
